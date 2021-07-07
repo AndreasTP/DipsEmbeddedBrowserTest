51f6302d63b684872ba9766c7a3c3cbf98447e36
Thor Stenbæk <tst@dips.no>
2021-07-07 09:29:15 +0200

    Created WPF app

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using EmbeddedBrowserTest.ViewModels;
+using System.Windows;
+
+namespace EmbeddedBrowserTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+
+            MainWindow = mainView;
+            MainWindow.Show();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmbeddedBrowserTest.ViewModels
+{
+    public class MainViewModel
+    {
+        public string Title { get; }
+
+        public MainViewModel()
+        {
+            Title = "EmbeddedBrowser Test App";
+        }
+    }
+}
