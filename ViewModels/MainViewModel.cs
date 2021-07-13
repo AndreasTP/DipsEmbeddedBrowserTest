b54d19a0d35bd8dd688e5f48bfff319420ccd42e
AndreasTP <67000923+AndreasTP@users.noreply.github.com>
2021-07-13 09:17:11 +0200

    Andreas (#3)
    * noe
    
    * input gets saved
    
    Co-authored-by: Andreas <anpe@dips.no>

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,8 +1,15 @@
 ﻿using DIPS.Arena.UI.Commands;
 using DIPS.EmbeddedBrowser.Contracts;
 using DIPS.EmbeddedBrowser.Contracts.Builders;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
 using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace EmbeddedBrowserTest.ViewModels
 {
@@ -13,55 +20,90 @@ namespace EmbeddedBrowserTest.ViewModels
         public string Title { get; }
         
         private string m_documentStatus;
+        private string m_documentComment;
+        private SolidColorBrush m_borderColor;
         public string Url { get; set; } = "http://localhost:9000";
         public IEmbeddedBrowserViewModel EmbeddedBrowser { get; }
 
         public ICommand NavigateCommand { get; }
 
         public ICommand SaveCommand { get; }
-        public ICommand SendScriptCommand { get; }
+
+        public ICommand ApproveCommand { get; }
 
         public MainViewModel(IEmbeddedBrowserBuilder embeddedBrowserBuilder)
         {
             EmbeddedBrowserBuilder = embeddedBrowserBuilder;
             Title = "EmbeddedBrowser Test App";
-            DocumentStatus = "Not dirty";
+            DocumentStatus = "Clean document";
+            DocumentComment = "Text: ";
             
-            EmbeddedBrowser = EmbeddedBrowserBuilder.WithJavaScriptBindings(new Proxy(this)).Build();
+            BorderColor = System.Windows.Media.Brushes.LawnGreen;
+            
+            EmbeddedBrowser = EmbeddedBrowserBuilder.WithJavaScriptBindings(new WebAppProxy(this)).Build();
             EmbeddedBrowser.LoadAsync(new System.Uri(Url));
 
             NavigateCommand = new DelegateCommand(
                 async () => 
                 {
-                    await EmbeddedBrowser.LoadAsync(new System.Uri(Url));
-                    
+                    await EmbeddedBrowser.LoadAsync(new System.Uri(Url));                   
 
                 });
 
-            SaveCommand = new DelegateCommand(()=>
-            {
-                EmbeddedBrowser.ExecuteJavascriptAsync("var comment = document.getElementById('commentBox');" +
-                                           "console.log(comment.value);");
-            });
-            SendScriptCommand = new DelegateCommand(
-                  () =>
+            SaveCommand = new DelegateCommand(
+                async ()=>
                 {
-                    EmbeddedBrowser.ExecuteJavascriptAsync("var comment = document.getElementById('commentBox');" +
-                                           "console.log(comment.value);");
+                    var comment = await GetCommentFromWebApp();
+                    DocumentComment = "Text: " + comment;
+                    BorderColor = System.Windows.Media.Brushes.LawnGreen;
+                    DocumentStatus = "Clean document";
+
+                    //await EmbeddedBrowser.ExecuteJavascriptAsync("const newPatient = {" +
+                    //     "method: 'PUT'," +
+                    //     "body: JSON.stringify(FHIR_Patient)," +
+                    //     "headers: { 'Content-Type': 'application/json',}}" +
+                    //     "newPatient.address = address;" +
+                    //     "client.request('Patient/3458');");
+
+                    //await EmbeddedBrowser.ExecuteJavascriptAsync("updatePatient()");
+                    //await EmbeddedBrowser.ExecuteJavascriptAsync("document.getElementById('hei').msgprint()");
+                    await EmbeddedBrowser.ExecuteJavascriptAsync("document.getElementById('SaveDocument').click()");
+
+                    HardCodedAuthorizationMessageHandler handler = new HardCodedAuthorizationMessageHandler
+                    {
+                        AuthTicket = "auth-ticket",
+                        authTicketCode = "fad3f55a-b3a3-455f-80b0-bd4f82c29bf6"
+                    };
+
+                    var fhirClient = new FhirClient("https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4/", messageHandler: handler);
+                    var patientBundle = await fhirClient.SearchAsync<Patient>(new string[] { $"identifier={13116900216}" });
+
                 });
+
+            ApproveCommand = new DelegateCommand(()=> ApproveDocument());
+        }      
+
+        private async void ApproveDocument()
+        {
+            var comment = await GetCommentFromWebApp();
+            DocumentComment = "Text: " + comment;
+            BorderColor = System.Windows.Media.Brushes.Yellow;
+            DocumentStatus = "Approved document";
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged(string propertyName)
+        public void UpdateDocumentStatus()
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            DocumentStatus = "Dirty document";
+            BorderColor = System.Windows.Media.Brushes.Red;
         }
 
-        public void ChangeDocumentStatus()
+        private async Task<string> GetCommentFromWebApp()
         {
-            DocumentStatus = "Dirty";
+            var s = await EmbeddedBrowser.EvaluateJavaScriptAsync("function foo() {" +
+                                                            "var comment = document.getElementById('commentBox');" +
+                                                            "return comment.value; }" +
+                                                            "foo();");
+            return s?.Result?.ToString();
         }
 
         public string DocumentStatus
@@ -74,6 +116,45 @@ namespace EmbeddedBrowserTest.ViewModels
             } 
         }
 
-        
+        public string DocumentComment
+        {
+            get => m_documentComment;
+            set
+            {
+                m_documentComment = value;
+                OnPropertyChanged("DocumentComment");
+            }
+        }
+
+        public SolidColorBrush BorderColor
+        {
+            get => m_borderColor;
+            set
+            {
+                m_borderColor = value;
+                OnPropertyChanged("BorderColor");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+    public class HardCodedAuthorizationMessageHandler : HttpClientHandler
+    {
+        public string AuthTicket { get; set; }
+        public string authTicketCode { get; set; }
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add(AuthTicket, authTicketCode);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
     }
 }
